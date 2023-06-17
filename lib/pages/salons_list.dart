ee4cb76e08260a8fa47ef6a94bde9916410292c5
Angela Ampova <angela.ampova@students.finki.ukim.mk>
2023-06-17 16:58:36 +0200

    small changes

--- lib/pages/salons_list.dart
+++ lib/pages/salons_list.dart
@@ -10,68 +10,85 @@ class SalonsListPage extends StatelessWidget {
     'Studio Metier',
     'Sashions',
     'Sim Saltirova',
-    'Nails M',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Column(
-        children: [
-          // Header
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.all(20.0),
-            child: Text(
-              'Beauty Salons',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Back Button
+            Container(
+              margin: EdgeInsets.only(left: 10.0, top: 10.0),
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.teal,
+                ),
+                onPressed: () {
+                  Navigator.pop(context); // Navigate back to the previous screen
+                },
               ),
             ),
-          ),
-          // Search Bar
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Row(
-              children: [
-                Center(
-                  child: Icon(Icons.search),
-                ),
-                SizedBox(width: 10.0),
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      'Search salons',
-                      style: TextStyle(color: Colors.grey),
-                    ),
+            // Header
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Beauty Salons',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-          // Salons List
-          Expanded(
-            child: ListView.separated(
-              padding: EdgeInsets.all(10.0),
-              itemCount: salonNames.length,
-              separatorBuilder: (BuildContext context, int index) =>
-                  Divider(color: Colors.grey),
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(salonNames[index]),
-                );
-              },
+            // Search Bar
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Row(
+                children: [
+                  Center(
+                    child: Icon(Icons.search),
+                  ),
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Search salons',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            // Salons List
+            Expanded(
+              child: ListView.separated(
+                padding: EdgeInsets.all(10.0),
+                itemCount: salonNames.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                    Divider(color: Colors.grey),
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(salonNames[index]),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
