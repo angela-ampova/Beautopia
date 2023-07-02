2ba6844607b587889c4faa0f0344b252fde04827
Viktorija Ampova <viktorija.ampova@students.finki.ukim.mk>
2023-07-02 13:52:24 +0200

    changes in the layout

--- lib/pages/book.dart
+++ lib/pages/book.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:beautopia_project/main.dart';
 
 class BookPage extends StatefulWidget {
   @override
@@ -10,8 +10,14 @@ class BookPage extends StatefulWidget {
 class _BookPageState extends State<BookPage> {
   String? _selectedService;
   DateTime? _selectedDate;
-  DateTime? _selectedTime;
+  TimeOfDay? _selectedTime;
   String? selectedFilePath; // Define selectedFilePath variable
+
+  final Map<String, double> servicePrices = {
+    'Hair': 1500,
+    'Makeup': 2500,
+    'Nails': 1000,
+  };
 
   Future<void> _uploadFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -76,7 +82,7 @@ class _BookPageState extends State<BookPage> {
                     ),
                     SizedBox(width: 20.0),
                     Text(
-                      'Salon Name',
+                      'Sashions',
                       style: TextStyle(
                         fontSize: 18.0,
                         fontWeight: FontWeight.bold,
@@ -108,7 +114,7 @@ class _BookPageState extends State<BookPage> {
                   hintText: 'Select',
                   hintStyle: TextStyle(color: Colors.black),
                   enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.teal),
+                    borderSide: BorderSide(color: Colors.black),
                   ),
                 ),
                 value: _selectedService,
@@ -125,150 +131,155 @@ class _BookPageState extends State<BookPage> {
               ),
             ),
             SizedBox(height: 20.0),
-            // Date and Time Pickers
+            // Date and Time Buttons
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _selectDate(context); // Open Date Picker
+                      },
+                      child: Text(
+                        'Select Date',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.teal),
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _selectTime(context); // Open Time Picker
+                      },
+                      child: Text(
+                        'Select Time',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.teal),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            // Selected Date and Time
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Select Date and Time',
+                    'Selected Date:',
                     style: TextStyle(
                       fontSize: 16.0,
                       color: Colors.teal,
                     ),
                   ),
                   SizedBox(height: 8.0),
-                  TextButton.icon(
-                    onPressed: () {
-                      DatePicker.showDatePicker(
-                        context,
-                        showTitleActions: true,
-                        onConfirm: (date) {
-                          setState(() {
-                            _selectedDate = date;
-                          });
-                        },
-                        currentTime: _selectedDate ?? DateTime.now(),
-                        locale: LocaleType.en,
-                      );
-                    },
-                    icon: Icon(
-                      Icons.calendar_today,
-                      color: Colors.black,
+                  Text(
+                    _selectedDate != null
+                        ? _selectedDate.toString().split(' ')[0] // Show only the date
+                        : 'No date selected',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: _selectedDate != null ? Colors.black : Colors.red,
+                      fontWeight: _selectedDate != null ? FontWeight.normal : FontWeight.bold,
                     ),
-                    label: Text(
-                      _selectedDate != null
-                          ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
-                          : 'Select Date',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.teal,
-                      ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Selected Time:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.teal,
                     ),
                   ),
                   SizedBox(height: 8.0),
-                  TextButton.icon(
-                    onPressed: () {
-                      DatePicker.showTimePicker(
-                        context,
-                        showTitleActions: true,
-                        onConfirm: (time) {
-                          setState(() {
-                            _selectedTime = time;
-                          });
-                        },
-                        currentTime: _selectedTime ?? DateTime.now(),
-                        locale: LocaleType.en,
-                      );
-                    },
-                    icon: Icon(
-                      Icons.access_time,
-                      color: Colors.black,
-                    ),
-                    label: Text(
-                      _selectedTime != null
-                          ? '${_selectedTime!.hour}:${_selectedTime!.minute}'
-                          : 'Select Time',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.teal,
-                      ),
+                  Text(
+                    _selectedTime != null
+                        ? '${_selectedTime!.hour}:${_selectedTime!.minute}' // Display selected time only
+                        : 'No time selected',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: _selectedTime != null ? Colors.black : Colors.red,
+                      fontWeight: _selectedTime != null ? FontWeight.normal : FontWeight.bold,
                     ),
                   ),
                 ],
+              ),
+            ),
+
+            SizedBox(height: 20.0),
+            // Select Service Label
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Text(
+                'Upload Inspiration',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.teal,
+                ),
               ),
             ),
             SizedBox(height: 20.0),
             // Upload Button
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               child: ElevatedButton(
                 onPressed: _uploadFile,
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.teal,
-                  onPrimary: Colors.white,
-                  padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
                 child: Text(
-                  'Upload an inspiration from device',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
+                  'Upload from device',
+                  style: TextStyle(color: Colors.white),
                 ),
+                style: ElevatedButton.styleFrom(primary: Colors.teal),
               ),
             ),
             SizedBox(height: 20.0),
             // Total Price
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Total Price',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.teal,
+            if (_selectedService != null)
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Price:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.teal,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8.0),
-                  Text(
-                    '2000 MKD', // Replace with actual price value
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                    SizedBox(height: 8.0),
+                    Text(
+                      '${servicePrices[_selectedService!]} MKD',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
             SizedBox(height: 20.0),
             // Finish Button
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Container(
-                margin: EdgeInsets.only(right: 20.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Align(
+                alignment: Alignment.centerRight,
                 child: ElevatedButton(
-                  onPressed: _finishBooking,
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.teal,
-                    onPrimary: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
+                  onPressed: () {
+                    // Perform action on finish button press
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+                  },
                   child: Text(
                     'Finish',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
+                    style: TextStyle(color: Colors.white),
                   ),
+                  style: ElevatedButton.styleFrom(primary: Colors.teal),
                 ),
               ),
             ),
@@ -284,13 +295,37 @@ class _BookPageState extends State<BookPage> {
     });
   }
 
-  void _uploadImage() {
-    // Handle image upload
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(Duration(days: 500)),
+    );
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
   }
 
-  void _finishBooking() {
-    // Handle finish booking
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (pickedTime != null) {
+      setState(() {
+        _selectedTime = pickedTime;
+      });
+    }
   }
+
+  List<String> get _serviceOptions => servicePrices.keys.toList();
 }
 
-final List<String> _serviceOptions = ['Hair', 'Makeup', 'Nails'];
+void main() {
+  runApp(MaterialApp(
+    home: BookPage(),
+  ));
+}
